1c4d00fb42f35e56122bfe81ef912863cd179557
Sequoia <91288286+3equoia@users.noreply.github.com>
2024-08-04 20:06:46 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const writer = async (text, textElem) => {
     for (let index = 0; index < text.length; index++) {
         const letter = text[index]
         textElem.innerHTML += letter
-        await wait(50)
+        await wait(100)
     }
 }
 writer(helloWorld, textElement)
